1812fd86921220045265c69d4c3712e212981ee8
Vilnis2K <79080062+Vilnius-ITB@users.noreply.github.com>
2023-05-16 02:38:47 +0300

    Update

--- app/src/androidTest/java/io/github/VilniusITB/BakalauraPraktiskais/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/github/VilniusITB/BakalauraPraktiskais/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("io.github.VilniusITB.BakalauraPraktiskais", appContext.getPackageName());
+        assertEquals("io.github.VilniusITB.terminal", appContext.getPackageName());
     }
 }
